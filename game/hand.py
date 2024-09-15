eba4867fa236d498682f59df3382fb0d39c0f030
Chancellor Lam <l2.chancellor@gmail.com>
2024-09-15 16:08:14 -0400

    Created hand class

---
+++ game/hand.py
@@ -0,0 +1,24 @@
+from game.card_collection import CardCollection
+from game.card_shoe import CardShoe
+
+
+class Hand(CardCollection):
+
+    def sum_hand(self):
+        lower_sum = 0
+        higher_sum = 0
+        num_aces = 0
+
+        for card in self.cards:
+            if card.get_card() == "A":
+                num_aces = num_aces + 1
+            higher_sum = higher_sum + card.get_value()
+
+        while num_aces > 0 and higher_sum > 21:
+            num_aces = num_aces - 1
+            higher_sum = higher_sum - 10
+
+        if num_aces > 0 and higher_sum - 10 > 0:
+            lower_sum = higher_sum - 10
+        return [lower_sum, higher_sum]
+
