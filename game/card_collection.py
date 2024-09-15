8f2fa54e9241d386e3ae2fec35036dfa6c298711
Chancellor Lam <l2.chancellor@gmail.com>
2024-09-15 01:31:02 -0400

    Added __repr__ method for card_collection class

--- game/card_collection.py
+++ game/card_collection.py
@@ -7,6 +7,11 @@ class CardCollection(ABC):
     def __init__(self):
         self.cards = []
 
+    def __repr__(self):
+        cards = [str(card.get_card()) for card in self.cards]
+        formatted_output = "[" + ", ".join(cards) + "]"
+        return formatted_output
+
     def get_top_card(self):
         return self.cards[0]
 
