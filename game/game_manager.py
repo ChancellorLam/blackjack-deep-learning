cb96418a8f7e99316dbef12c3d880cca48f684d2
ChancellorLam <135484498+ChancellorLam@users.noreply.github.com>
2024-09-20 14:54:26 -0400

    Created game_manager class

---
+++ game/game_manager.py
@@ -0,0 +1,9 @@
+from blackjack_entities.card_shoe import CardShoe
+
+
+class GameManager:
+    def __init__(self):
+        player_money = 500
+        current_bet = 15
+        card_shoe = CardShoe()
+
