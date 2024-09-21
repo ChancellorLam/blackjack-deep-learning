15e0ed70c73659739e68521288f56e82588a1847
Chancellor Lam <l2.chancellor@gmail.com>
2024-09-21 16:59:17 -0400

    Added method for populating game_deck in game_manager class

--- game/game_manager.py
+++ game/game_manager.py
@@ -6,3 +6,6 @@ class GameManager:
         self.player_money = 500
         self.current_bet = 15
         self.game_deck = Deck()
+
+    def load_game_deck_with_standard_decks(self, num_decks):
+        self.game_deck.add_standard_decks(num_decks)
