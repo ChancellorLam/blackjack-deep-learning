f6f87c441313575820e61b4a10e8c3a21f936318
Chancellor Lam <l2.chancellor@gmail.com>
2024-09-22 19:16:55 -0400

    Refactor player hands to support multiple hands per player and finish deal_starting_cards() method

--- game/blackjack_game.py
+++ game/blackjack_game.py
@@ -4,7 +4,7 @@ from blackjack_entities.hand import Hand
 class BlackjackGame:
     def __init__(self):
         self.dealer_hand = Hand()
-        self.player_hands = {"Player 1": Hand()}
+        self.player_hands = {"Player 1": [Hand()]}
 
     def add_player(self):
-        self.player_hands["Player " + str(len(self.player_hands) + 1)] = Hand()
+        self.player_hands["Player " + str(len(self.player_hands) + 1)] = [Hand()]
--- game/game_manager.py
+++ game/game_manager.py
@@ -1,6 +1,5 @@
 from blackjack_entities.deck import Deck
 
-
 class GameManager:
     def __init__(self):
         self.player_money = 500
@@ -10,7 +9,8 @@ class GameManager:
     def load_game_deck_with_standard_decks(self, num_decks):
         self.game_deck.add_standard_decks(num_decks)
 
-    def deal_cards(self, current_game):
-        current_game.dealer_hand.add_card(self.game_deck.pop_left_card())
-        for hand in current_game.player_hands.values():
-            hand.add_card(self.game_deck.pop_left_card())
+    def deal_starting_cards(self, current_game):
+        for i in range(0, 2):
+            for first_hand in current_game.player_hands.values():
+                first_hand[0].add_card(self.game_deck.pop_left_card())
+            current_game.dealer_hand.add_card(self.game_deck.pop_left_card())
