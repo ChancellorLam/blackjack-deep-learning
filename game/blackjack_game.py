02d26693ad298a50cd2bc0176caaf6447e019897
Chancellor Lam <l2.chancellor@gmail.com>
2024-09-22 14:12:20 -0400

    Added add_player method to support dynamic player addition

--- game/blackjack_game.py
+++ game/blackjack_game.py
@@ -5,3 +5,6 @@ class BlackjackGame:
     def __init__(self):
         self.dealer_hand = Hand()
         self.player_hands = {"Player 1": Hand()}
+
+    def add_player(self):
+        self.player_hands["Player " + str(len(self.player_hands) + 1)] = Hand()
