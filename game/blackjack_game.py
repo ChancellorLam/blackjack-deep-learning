2978f52baa8fb77045c8620854a49db3b730afc5
Chancellor Lam <l2.chancellor@gmail.com>
2024-09-22 20:43:29 -0400

    Implement dynamic player decision prompt and ability to get_player_decision

--- game/blackjack_game.py
+++ game/blackjack_game.py
@@ -1,10 +1,32 @@
 from blackjack_entities.hand import Hand
+from menu_tools.menus import selection_menu
 
 
 class BlackjackGame:
     def __init__(self):
         self.dealer_hand = Hand()
         self.player_hands = {"Player 1": [Hand()]}
+        self.turn = 0
+
+    def get_player_decision(self, player_num):
+        prompt_with_decisions = self.construct_prompt_based_on_game_state(player_num)
+        prompt = prompt_with_decisions[0]
+        decisions = prompt_with_decisions[1]
+        return selection_menu(prompt, decisions)
 
     def add_player(self):
         self.player_hands["Player " + str(len(self.player_hands) + 1)] = [Hand()]
+
+    def construct_prompt_based_on_game_state(self, player_num):
+        prompt = "Would you like to Hit"
+        decisions = ["Stand"]
+        if self.turn == 0:
+            decisions.append("Double")
+            if self.player_hands["Player " + str(player_num)][0].can_split():
+                decisions.append("Split")
+
+        if len(decisions) == 1:
+            return f"{prompt} or {decisions[0]}?", decisions
+
+        # Join decisions with commas and 'or' for the last item
+        return f"{prompt}, " + ", ".join(decisions[:-1]) + f", or {decisions[-1]}?", decisions
