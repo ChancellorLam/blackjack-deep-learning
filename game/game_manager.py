4359aba8fedd2a1b6cb5149a3566e3e96ce9c073
ChancellorLam <135484498+ChancellorLam@users.noreply.github.com>
2024-09-26 14:57:43 -0400

    Added check_and_process_blackjack_results method to GameManager class

--- game/game_manager.py
+++ game/game_manager.py
@@ -13,3 +13,17 @@ class GameManager:
         for i in range(0, 2):
             current_game.player_hands[0].add_card(self.game_deck.pop_left_card())
             current_game.dealer_hand.add_card(self.game_deck.pop_left_card())
+
+    def check_and_process_blackjack_results(self, current_game, blackjack_ratio):
+        print("Dealer's Hand: " + str(current_game.dealer_hand))
+        if current_game.player_hands[0].has_blackjack() and current_game.dealer_hand.has_blackjack():
+            print("Push!")
+        elif current_game.player_hands[0].has_blackjack():
+            self.player_money = self.player_money + self.current_bet * blackjack_ratio
+            print("Player Blackjack! You win!")
+        elif current_game.dealer_hand.has_blackjack():
+            self.player_money = self.player_money - self.current_bet
+            print("Dealer has Blackjack! You lose!")
+        else:
+            return False
+        return True
