5d9ba9ab761694e805a8e1f8b7517199c6eff30e
Chancellor Lam <l2.chancellor@gmail.com>
2024-09-29 23:53:14 -0400

    Refactored hand.py
    -Added instance variable linking a bet to a specific hand in constructor
    -Changed the sum_hand method by changing num_aces to
     num_aces_counting_as_eleven
    -Added optional parameter to sum_hand method to select between hard sum
     only or both hard and soft sums.
    -Improved variable naming in sum_hand method for better code clarity
    -Refactored is_bust() to adequately handle input from sum_hand
    -Added method to double_bet associated with hand

--- blackjack_entities/hand.py
+++ blackjack_entities/hand.py
@@ -1,23 +1,30 @@
 from blackjack_entities.card_collection import CardCollection
 
 class Hand(CardCollection):
+    def __init__(self, bet=None):
+        super().__init__()
+        self.bet = bet
 
-    def sum_hand(self):
+    def sum_hand(self, hard_sum_only=False):
         hard_sum = 0
-        num_aces = 0
+        num_aces_counting_as_eleven = 0
 
         for card in self.cards:
             if card.get_card() == "A":
-                num_aces = num_aces + 1
+                num_aces_counting_as_eleven = num_aces_counting_as_eleven + 1
             hard_sum = hard_sum + card.get_value()
 
-        while num_aces > 0 and hard_sum > 21:
-            num_aces = num_aces - 1
+        while num_aces_counting_as_eleven > 0 and hard_sum > 21:
+            num_aces_counting_as_eleven = num_aces_counting_as_eleven - 1
             hard_sum = hard_sum - 10
 
-        if num_aces > 0 and hard_sum - 10 > 0:
-            soft_sum = (hard_sum - 10, hard_sum)
-            return soft_sum
+        if not hard_sum_only:
+            can_count_as_soft_sum = num_aces_counting_as_eleven > 0 and hard_sum - 10 > 0
+
+            if can_count_as_soft_sum:
+                soft_sum = (hard_sum - 10, hard_sum)
+                return soft_sum
+
         return hard_sum
 
     def can_split(self):
@@ -27,5 +34,12 @@ class Hand(CardCollection):
         return self.cards[0].get_value() + self.cards[1].get_value() == 21
 
     def is_bust(self):
-        lower_sum, higher_sum = self.sum_hand()
-        return lower_sum > 21
+        hand_value = self.sum_hand()
+
+        if isinstance(hand_value, int):
+            return hand_value > 21
+        elif isinstance(hand_value, tuple) and len(hand_value) == 2:
+            return hand_value[0] > 21 and hand_value[1] > 21
+
+    def double_bet(self):
+        self.bet = self.bet * 2
