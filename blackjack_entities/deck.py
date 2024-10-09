be6d44647ae91043a45328b1bd89344eca679af2
Chancellor Lam <l2.chancellor@gmail.com>
2024-10-09 12:15:21 -0400

    Add used_cards Instance Variable to Deck Class
    -Introduced used_cards to track cards that have been played
    -Added restore_used_cards() method to return used cards to the deck

--- blackjack_entities/deck.py
+++ blackjack_entities/deck.py
@@ -3,6 +3,9 @@ from blackjack_entities.card_collection import CardCollection
 
 
 class Deck(CardCollection):
+    def __init__(self):
+        super().__init__()
+        self.used_cards = []
 
     def add_standard_decks(self, num_decks):
         for i in range(0, num_decks):
@@ -10,3 +13,7 @@ class Deck(CardCollection):
                 for k in range(1, 14):
                     card = Card(k)
                     self.cards.append(card)
+
+    def restore_used_cards(self):
+        self.cards.extend(self.used_cards)
+        self.used_cards.clear()
